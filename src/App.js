ea8a5e9420ca795fe8e8cbf5cbc89a07262e56b7
mwao <vldzmwjdgus95@gmail.com>
2020-10-28 15:07:04 +0900

    message

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//body 태그에 해당하는 js 파일
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="grey-background">
+      <img src={logo} let="logo"/>
+      <h2>
+        Let's React
+        </h2>  
+    </div>
+  );
+}
+
+export default App;
