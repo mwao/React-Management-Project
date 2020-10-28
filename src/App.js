43224a48301f54b98ee931fc8faafffafb65504a
mwao <vldzmwjdgus95@gmail.com>
2020-10-28 15:42:39 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 //body 태그에 해당하는 js 파일
 
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers=[{
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'Cindy',
+  'birthday':'111111',
+  'gender':'Women',
+  'job':'Actor'
+
+},{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'Boo',
+  'birthday':'222222',
+  'gender':'Men',
+  'job':'Actor'
+
+},{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name':'Apple',
+  'birthday':'333333',
+  'gender':'Women',
+  'job':'Fruits'
+
+}
+
+]
 
 function App() {
   return (
-    <div className="grey-background">
-      <img src={logo} let="logo"/>
-      <h2>
-        Let's React
-        </h2>  
-    </div>
+    // <Customer
+    //   id={customer.id}
+    //   image={customer.image}
+    //   name={customer.name}
+    //   birthday={customer.birthday}
+    //   gender={customer.gender}
+    //   job={customer.job}
+    // />
+<div>{customers.map(c=>{
+  return(
+    <Customer
+      key={c.id}
+      id={c.id}
+      image={c.image}
+      name={c.name}
+      birthday={c.birthday}
+      gender={c.gender}
+      job={c.job}
+
+/>)})}
+</div>
+
   );
 }
 
